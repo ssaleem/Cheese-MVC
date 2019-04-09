dbed06a79953216731a1b896568cb41b6ff41608
ssaleem <sara@sarasaleem.com>
2019-04-09 10:16:19 -0400

    Add remove cheese functionality through checkboxes

--- src/main/java/com/springThyme/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/springThyme/cheesemvc/controllers/CheeseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import utils.CheeseNameValidator;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -15,14 +16,12 @@ import java.util.HashMap;
 public class CheeseController {
 
     private static HashMap<String, String> cheeses= new HashMap<>();
-//    static {
-//        cheeses.add("Cheddar");
-//    }
 
     //do not use @ResponseBody annotation with templates
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model){
+
         model.addAttribute("title", "My Cheeses");
         model.addAttribute("cheeses", cheeses);
         return "cheese/index";
@@ -61,6 +60,7 @@ public class CheeseController {
 
     @RequestMapping(value = "remove")
     public String displayRemoveCheeseForm(Model model){
+
         model.addAttribute("cheeseList", cheeses.keySet());
         return "/cheese/remove";
 
@@ -68,10 +68,39 @@ public class CheeseController {
 
     @RequestMapping(value = "remove", method = RequestMethod.POST)
     public String processRemoveCheeseForm(
-            @RequestParam String cheeseSelection){
+            @RequestParam(required = false) String cheeseSelection){
 
-        cheeses.remove(cheeseSelection);
+        if(!cheeseSelection.equals("")){
+            cheeses.remove(cheeseSelection);
+        }
+
         return "redirect:";
     }
+
+    @RequestMapping(value = "removeMultiple")
+    public String displayRemoveMultipleCheeseForm(Model model){
+
+        model.addAttribute("cheeses", cheeses);
+        return "/cheese/removeMultiple";
+
+    }
+
+//    to protect from error caused by absence of a parameter, do 2 things
+//    1. make required = false in annotation
+//    2. check for null within method
+    @RequestMapping(value = "removeMultiple", method = RequestMethod.POST)
+    public String processRemoveMultipleCheeseForm(
+            @RequestParam(required = false) ArrayList<String> cheese){
+
+        if(cheese != null){
+            for(String cheeseName: cheese){
+                cheeses.remove(cheeseName);
+            }
+        }
+
+        return "redirect:";
+    }
+
+
 
 }
