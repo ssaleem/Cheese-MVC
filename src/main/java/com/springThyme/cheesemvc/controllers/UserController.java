6dc8ff029f19bca18676a769cc4cdadc062632e7
ssaleem <sara@sarasaleem.com>
2019-04-15 17:02:58 -0400

    Add validation to user signup form

--- src/main/java/com/springThyme/cheesemvc/controllers/UserController.java
+++ src/main/java/com/springThyme/cheesemvc/controllers/UserController.java
@@ -4,10 +4,13 @@ import com.springThyme.cheesemvc.models.User;
 import com.springThyme.cheesemvc.models.data.UserManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "user")
@@ -20,23 +23,32 @@ public class UserController {
     }
 
     @RequestMapping(value = "add")
-    public String add(){
+    public String add(Model model){
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAdd(Model model, @ModelAttribute User user, String verify){
-        if(user.getPassword().equals(verify)) {
-            UserManager.addUser(user);
-            return "redirect:";
-        }
-        else {
-            model.addAttribute("name", user.getName());
-            model.addAttribute("email", user.getEmail());
+    public String processAdd(
+            Model model,
+            @ModelAttribute @Valid User user,
+            Errors errors,
+            String verify){
+
+        boolean verifyError = false;
+        if(verify == null ||  !user.getPassword().equals(verify)) {
+            user.setPassword("");
             model.addAttribute("error", "Password does not match");
+            verifyError = true;
+        }
+
+        if(errors.hasErrors() || verifyError){
+//            model.addAttribute(user);
             return "user/add";
         }
 
+        UserManager.addUser(user);
+        return "redirect:";
     }
 
     @RequestMapping(value="{id}")
--- src/main/java/com/springThyme/cheesemvc/models/User.java
+++ src/main/java/com/springThyme/cheesemvc/models/User.java
@@ -1,5 +1,9 @@
 package com.springThyme.cheesemvc.models;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.Objects;
 
@@ -8,9 +12,17 @@ public class User {
     private static int nextID = 0;
 
     private int ID;
+    @NotNull
+    @Size(min = 5, max = 15, message="Name must be between 5 and 15 characters")
     private String name;
+
+    @Email
     private String email;
+
+    @NotNull
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
+
     private Date joinDate;
 
     public User() {
